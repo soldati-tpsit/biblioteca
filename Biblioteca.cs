5e9a33d586d70a81e0e928df59986af5bd0e5654
Elia Soldati <eliasol2005@gmail.com>
2022-11-12 08:57:30 +0100

    Implementazione iniziale classe Biblioteca

---
+++ Biblioteca.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    internal class Biblioteca
+    {
+        private string _nome;
+        private string _indirizzo;
+        private TimeOnly _orarioApertura, _orarioChiusura;
+        private Dictionary<string, Libro> _libri = new();
+        private Dictionary<string, List<Libro>> _autori = new();
+
+        public Biblioteca(string nome, string indirizzo, TimeOnly orarioApertura, TimeOnly orarioChiusura)
+        {
+            _nome = nome;
+            _indirizzo = indirizzo;
+            _orarioApertura = orarioApertura;
+            _orarioChiusura = orarioChiusura;
+        }
+
+        public string Nome { get => _nome; set => _nome = value; }
+        public string Indirizzo { get => _indirizzo; set => _indirizzo = value; }
+        public TimeOnly OrarioApertura { get => _orarioApertura; set => _orarioApertura = value; }
+        public TimeOnly OrarioChiusura { get => _orarioChiusura; set => _orarioChiusura = value; }
+    }
+}
